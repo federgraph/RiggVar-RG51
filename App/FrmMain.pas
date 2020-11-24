dd36ea2e6f84be740521b8a32bb1242b1b22e9c7
Gustav Schubert <gschub@googlemail.com>
2020-11-24 10:34:45 +0100

    RG Model cleanup

--- App/FrmMain.pas
+++ App/FrmMain.pas
@@ -326,7 +326,7 @@ begin
   TKR := Round(TransKreisRadius * FScale);
 
   SpeedPanelHeight := Raster - Round(FScale * Margin);
-  ListboxWidth := Round(200 * FScale);
+  ListboxWidth := Round(230 * FScale);
 
   CreateComponents;
 
@@ -337,7 +337,7 @@ begin
   SetupListbox(ReportListbox);
 
   Rigg := TRigg.Create;
-  Rigg.TrimmTab.FScale := FScale;
+  Rigg.TrimmTabelle.FScale := FScale;
   Rigg.ControllerTyp := ctOhne;
 
   Main := TMain.Create(Rigg);
@@ -1478,8 +1478,8 @@ begin
     ControllerGraph.ControllerTyp := Rigg.ControllerTyp;
     ControllerGraph.ControllerPos := Round(Main.ParamValue[fpController]);
     ControllerGraph.ParamXE := Rigg.MastPositionE;
-    ControllerGraph.ParamXE0 := Round(Rigg.rP.E0.X - Rigg.rP.D0.X);
-    ControllerGraph.EdgePos := Round(Rigg.GSB.Find(fpController).Min);
+    ControllerGraph.ParamXE0 := Round(Rigg.GetPoint3D(ooE0).X - Rigg.GetPoint3D(ooD0).X);
+    ControllerGraph.EdgePos := Round(Rigg.RggFA.Find(fpController).Min);
 
     ControllerGraph.Draw(TFigure.dtController);
   end;
@@ -1754,7 +1754,7 @@ begin
     Top := 30;
     Width := 1700;
     Height := 960;
-    ReportMemoWidth := Round(480 * FScale);
+    ReportMemoWidth := Round(500 * FScale);
   end
   else
   begin
